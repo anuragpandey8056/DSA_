49913440dbc61175d37b4cf052f8c7126e1efff9
Anurag pandey <163901656+anuragpandey8056@users.noreply.github.com>
2026-09-22 21:50:41 +0530

    Time: 11 ms (39.66%) | Memory: 16.1 MB (5.89%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+//two sum
+    vector<int> twoSum(vector<int>& nums, int target) {
+      
+       unordered_map<int ,int>m;
+       vector<int>ans;
+       for(int i=0;i<nums.size();i++){
+           int first=nums[i];
+           int second=target-first;
+           if(m.find(second)!=m.end()){    //yr mil jane ki condition hai
+                ans.push_back(i);
+                ans.push_back(m[second])  ;  
+           }
+           m[first]=i;
+          
+       }
+
+      return ans;
+        
+    }
+};
